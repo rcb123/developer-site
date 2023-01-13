bada0fd1f6255e26181b6336846298bbb0f2c8cc
Reza Banankhah <banankha@usc.edu>
2023-01-13 09:41:37 -0800

    Add DaisyUI and fix Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,11 @@
 <script>
 	import Header from '$components/Header.svelte';
-	import "../app.css";
+	import '../app.css';
+
+	/*
 	import { inject } from '@vercel/analytics';
 	inject();
+	*/
 </script>
 
 <title>Reza Banankhah</title>
