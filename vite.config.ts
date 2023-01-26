53f25192922052ff16b5e47f3fcb39143c97c67b
Reza Banankhah <banankha@usc.edu>
2023-01-25 17:30:15 -0800

    Small config changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import type { UserConfig } from 'vite';
 import path from 'path'
 
-/** @type {import('vite').UserConfig} */
-const config = {
+const config: UserConfig = {
 	plugins: [sveltekit()],
 	resolve: {
 		alias: {
