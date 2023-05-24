51ff766c36dc01d67f4a92c759afd3a0df3b566e
Reza Banankhah <banankha@usc.edu>
2023-05-24 01:26:19 -0700

    Updated Header.svelte
    Updated the header div to be a header element
    Updated the navigation sections to have the proper nav tag

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,14 +3,14 @@
 	import 'iconify-icon';
 </script>
 
-<div class="navbar h-[10vh] border-b bg-base-100">
+<header class="navbar h-[10vh] border-b bg-base-100">
 	<div class="navbar-start">
 		<a href="/" class="btn-ghost btn text-xl normal-case hover:bg-gray-200 hover:text-black">
 			<iconify-icon icon="mdi:code-braces" class="mr-2 text-4xl" />
 			Reza Banankhah
 		</a>
 	</div>
-	<div class="navbar-center hidden lg:flex">
+	<nav class="navbar-center hidden lg:flex">
 		<ul class="menu menu-horizontal px-1">
 			<li>
 				<a href="/projects" class="btn-ghost btn hover:bg-gray-200 hover:text-black">
@@ -31,9 +31,7 @@
 						>
 					</li>
 					<li>
-						<a href="https://hur.vercel.app/" class="hover:bg-gray-200 hover:text-black"
-							>Shortly</a
-						>
+						<a href="https://hur.vercel.app/" class="hover:bg-gray-200 hover:text-black">Shortly</a>
 					</li>
 					<li>
 						<a href="/projects/maze" class="hover:bg-gray-200 hover:text-black">Maze Solver</a>
@@ -52,9 +50,9 @@
 				<a href="/contact" class="btn-ghost btn hover:bg-gray-200 hover:text-black">Contact</a>
 			</li>
 		</ul>
-	</div>
+	</nav>
 	<div class="navbar-end">
-		<div class="dropdown dropdown-end">
+		<div class="dropdown-end dropdown">
 			<div
 				class="flex flex-grow items-center justify-center gap-12 py-10 px-6 text-xl max-lg:hidden"
 			>
@@ -67,32 +65,34 @@
 			</div>
 			<!-- svelte-ignore a11y-label-has-associated-control -->
 			<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-			<label tabindex="0" class="btn-ghost btn hover:bg-gray-200 hover:text-black lg:hidden">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="h-5 w-5"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M4 6h16M4 12h8m-8 6h16"
-					/></svg
-				>
-			</label>
-			<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-			<ul
-				tabindex="0"
-				class="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow-md"
-			>
+			<nav>
+				<label tabindex="0" class="btn-ghost btn hover:bg-gray-200 hover:text-black lg:hidden">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						><path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M4 6h16M4 12h8m-8 6h16"
+						/></svg
+					>
+				</label>
 				<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
-				<li><a href="/projects" class="hover:bg-gray-200">Projects</a></li>
-				<li><a href="/resume" class="hover:bg-gray-200">Resume</a></li>
-				<li><a href="/about" class="hover:bg-gray-200">About</a></li>
-				<li><a href="/contact" class="hover:bg-gray-200">Contact</a></li>
-			</ul>
+				<ul
+					tabindex="0"
+					class="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow-md"
+				>
+					<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+					<li><a href="/projects" class="hover:bg-gray-200">Projects</a></li>
+					<li><a href="/resume" class="hover:bg-gray-200">Resume</a></li>
+					<li><a href="/about" class="hover:bg-gray-200">About</a></li>
+					<li><a href="/contact" class="hover:bg-gray-200">Contact</a></li>
+				</ul>
+			</nav>
 		</div>
 	</div>
-</div>
+</header>
