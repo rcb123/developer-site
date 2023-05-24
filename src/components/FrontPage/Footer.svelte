502fa264b29a07d31ae67173cffa138ef1270107
Reza Banankhah <banankha@usc.edu>
2023-05-24 01:27:41 -0700

    Updated footer
    Footer was unnecessary and lacked functionality
    Replaced text with social links

--- src/components/FrontPage/Footer.svelte
+++ src/components/FrontPage/Footer.svelte
@@ -1,3 +1,17 @@
-<div class="w-full bg-slate-700 text-base-100 py-4 text-center">
-    Developed and designed by Reza Banankhah
+<script lang="ts">
+	import Icon from '../Icon.svelte';
+</script>
+
+<div class="mb-5 flex w-full flex-col items-center justify-center space-y-6 py-2">
+	<div class="grid w-full grid-cols-3 divide-x divide-solid divide-blue-700/50">
+		<div class="flex flex-col items-center justify-center text-3xl text-blue-700">
+			<Icon icon="instagram" />
+		</div>
+		<div class="flex flex-col items-center justify-center text-3xl text-blue-700">
+			<Icon icon="github" />
+		</div>
+		<div class="flex flex-col items-center justify-center text-3xl text-blue-700">
+			<Icon icon="linkedin" />
+		</div>
+	</div>
 </div>
