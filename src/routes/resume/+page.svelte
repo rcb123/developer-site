7a999588560a5ce52d40f348b425681429132a62
Reza Banankhah <banankha@usc.edu>
2023-08-20 17:10:33 -0700

    Updated resume

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,6 +1,6 @@
 <div class="flex justify-center">
 	<iframe
-		src="https://drive.google.com/file/d/15tvMxamQv5gmuCZ7tQKGUp6Cqnz_FJ7y/preview"
+		src="https://drive.google.com/file/d/14Pi3nxK4-xZ6zjWhXvuJLf-E63_uQZOy/preview"
 		title="Resume"
 		allow="autoplay"
 		class="w-full sm:w-10/12 md:w-9/12 lg:w-3/4 h-[90vh]"
