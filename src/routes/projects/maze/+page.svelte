cc3a95718cdeccd33f84c92d1a39c413f948b96f
Reza Banankhah <banankha@usc.edu>
2023-11-02 19:19:02 -0700

    Updated maze solver
    Maze solver demo is now easier to use and allows the user to click on a grid to change cell states
    Also now allows for switching between BFS and DFS algorithms for solving the maze

--- src/routes/projects/maze/+page.svelte
+++ src/routes/projects/maze/+page.svelte
@@ -1,72 +1,44 @@
 <script lang="ts">
-	const invalidCharMessage = 'Error, invalid character.';
+	import { onMount } from 'svelte';
+
+	let algorithm = 'BFS';
+	let rows: number = 5;
+	let cols: number = 5;
+	let rowInputEl: HTMLInputElement;
+	let colInputEl: HTMLInputElement;
+
+	let maze: string[][] = Array.from({ length: rows }, () =>
+		Array.from({ length: cols }, () => '.')
+	);
+
 	const invalidMazeMessage = 'Invalid maze.';
 	const noPathMessage = 'No path could be found!';
 
-	let fileInput: HTMLInputElement;
-	let mazeInput: HTMLTextAreaElement;
-	let mazeOutput: HTMLDivElement;
-
-	function uploadMazeFile() {
-		if (!fileInput.files) return;
-		const file: File = fileInput.files[0];
-		const reader: FileReader = new FileReader();
-		reader.onload = () => {
-			const maze: string[][] = (reader.result as string).split('\n').map((row) => row.split(''));
-			console.log(maze);
-			const rows: number = maze.length;
-			const cols: number = maze[0].length;
-			const result = mazeSearch(maze, rows, cols);
-			if (result === 1) {
-				printMaze(maze);
-			} else if (result === 0) {
-				displayOutput(noPathMessage);
-			} else {
-				displayOutput(invalidMazeMessage);
-			}
-		};
-		reader.readAsText(file);
-	}
-
-	function processMazeInput() {
-		const input = mazeInput.value;
-		const maze = input.split('\n').map((row) => row.split(''));
+	function solve() {
 		const rows = maze.length;
 		const cols = maze[0].length;
-		const result = mazeSearch(maze, rows, cols);
-		if (result === 1) {
-			printMaze(maze);
-		} else if (result === 0) {
-			displayOutput(noPathMessage);
+		let result;
+		if (algorithm === 'BFS') {
+			result = mazeSearchBFS(maze, rows, cols);
+		} else if (algorithm === 'DFS') {
+			result = mazeSearchDFS(maze, rows, cols);
 		} else {
-			displayOutput(invalidMazeMessage);
+			alert('Invalid algorithm selected');
+			return;
+		}
+		if (result === 1) {
+			printMaze();
+		} else if (result === 0) {
+			alert(noPathMessage);
+		} else {
+			alert(invalidMazeMessage);
 		}
 	}
 
 	function mazeSearch(maze: string[][], rows: number, cols: number): number {
-		// Track number of start and finish locations
+		// Start and finish locations are fixed
 		let start: [number, number] = [0, 0];
-		let s: number = 0;
-		let finish: [number, number] = [0, 0];
-		let f: number = 0;
-
-		// Optimization 1: Use a single loop to check for start and finish locations
-		for (let i = 0; i < rows; i++) {
-			for (let j = 0; j < cols; j++) {
-				if (maze[i][j] === 'S') {
-					start = [i, j];
-					s++;
-				} else if (maze[i][j] === 'F') {
-					finish = [i, j];
-					f++;
-				}
-			}
-		}
-
-		// Maze only valid with a single start and finish location
-		if (s != 1 || f != 1) {
-			return -1;
-		}
+		let finish: [number, number] = [rows - 1, cols - 1];
 
 		// Declare directions for neighboring cells
 		const directions = [
@@ -93,7 +65,7 @@
 				// Check for finish
 				if (current[0] === finish[0] && current[1] === finish[1]) {
 					// Backtrack through predecessor array for shortest path
-					backtrack(maze, rows, cols, predecessor, start, finish);
+					backtrack(maze, cols, predecessor, start, finish);
 					return 1;
 				}
 
@@ -119,9 +91,120 @@
 		return 0;
 	}
 
+	function mazeSearchBFS(maze: string[][], rows: number, cols: number): number {
+		// Start and finish locations are fixed
+		let start: [number, number] = [0, 0];
+		let finish: [number, number] = [rows - 1, cols - 1];
+
+		// Declare directions for neighboring cells
+		const directions = [
+			[-1, 0],
+			[0, 1],
+			[1, 0],
+			[0, -1]
+		];
+
+		// Create arrays for previously explored cells and predecessor array for shortest path
+		const explored: boolean[] = Array(rows * cols).fill(false);
+		const predecessor: Array<[number, number]> = Array(rows * cols).fill([0, 0]);
+
+		// Start queue at the starting position
+		let queue: Array<[number, number]> = [];
+		queue.push(start);
+
+		// Keep going until finish is found or all possible locations have been visited
+		while (queue.length > 0) {
+			// Get current location
+			const current = queue.shift();
+
+			if (current) {
+				// Check for finish
+				if (current[0] === finish[0] && current[1] === finish[1]) {
+					// Backtrack through predecessor array for shortest path
+					backtrack(maze, cols, predecessor, start, finish);
+					return 1;
+				}
+
+				for (const direction of directions) {
+					const nextRow = current[0] + direction[0];
+					const nextCol = current[1] + direction[1];
+					const idx = nextRow * cols + nextCol;
+					if (
+						nextRow >= 0 &&
+						nextRow < rows &&
+						nextCol >= 0 &&
+						nextCol < cols &&
+						maze[nextRow][nextCol] !== '#' &&
+						!explored[idx]
+					) {
+						queue.push([nextRow, nextCol]);
+						explored[idx] = true;
+						predecessor[idx] = current;
+					}
+				}
+			}
+		}
+		return 0;
+	}
+
+	function mazeSearchDFS(maze: string[][], rows: number, cols: number): number {
+		// Start and finish locations are fixed
+		let start: [number, number] = [0, 0];
+		let finish: [number, number] = [rows - 1, cols - 1];
+
+		// Declare directions for neighboring cells
+		const directions = [
+			[-1, 0],
+			[0, 1],
+			[1, 0],
+			[0, -1]
+		];
+
+		// Create arrays for previously explored cells and predecessor array for shortest path
+		const explored: boolean[] = Array(rows * cols).fill(false);
+		const predecessor: Array<[number, number]> = Array(rows * cols).fill([0, 0]);
+
+		// Start stack at the starting position
+		let stack: Array<[number, number]> = [];
+		stack.push(start);
+
+		// Keep going until finish is found or all possible locations have been visited
+		while (stack.length > 0) {
+			// Get current location
+			const current = stack.pop();
+
+			if (current) {
+				// Check for finish
+				if (current[0] === finish[0] && current[1] === finish[1]) {
+					// Backtrack through predecessor array for shortest path
+					backtrack(maze, cols, predecessor, start, finish);
+					return 1;
+				}
+
+				for (const direction of directions) {
+					const nextRow = current[0] + direction[0];
+					const nextCol = current[1] + direction[1];
+					const idx = nextRow * cols + nextCol;
+					if (
+						nextRow >= 0 &&
+						nextRow < rows &&
+						nextCol >= 0 &&
+						nextCol < cols &&
+						maze[nextRow][nextCol] !== '#' &&
+						!explored[idx]
+					) {
+						stack.push([nextRow, nextCol]);
+						explored[idx] = true;
+						predecessor[idx] = current;
+					}
+				}
+			}
+		}
+		return 0;
+	}
+
 	function backtrack(
 		maze: string[][],
-		rows: number,
 		cols: number,
 		predecessor: [number, number][],
 		start: [number, number],
@@ -138,190 +221,120 @@
 		}
 	}
 
-	function printMaze(maze: string[][]) {
-		let output = '<table>';
-		for (let i = 0; i < maze.length; i++) {
-			output += '<tr class="text-sm">';
-			for (let j = 0; j < maze[i].length; j++) {
-				if (maze[i][j] === '*') {
-					output += `<td class="w-3 h-5 text-center p-1 text-red-500">${maze[i][j]}</td>`;
-				} else if (maze[i][j] === '.') {
-					output += `<td class="w-3 h-5 text-center p-1 text-white">${maze[i][j]}</td>`;
-				} else if (maze[i][j] === 'S' || maze[i][j] === 'F') {
-					output += `<td class="w-3 h-5 text-center p-1 text-blue-500">${maze[i][j]}</td>`;
+	function printMaze(): void {
+		let board: HTMLTableElement | null = document.getElementById('board') as HTMLTableElement;
+		if (!board) {
+			return;
+		}
+		board.innerHTML = '';
+		for (let rowNum = 0; rowNum < rows; rowNum++) {
+			let row = board.insertRow();
+			for (let colNum = 0; colNum < cols; colNum++) {
+				let cell = row.insertCell();
+				if (rowNum == 0 && colNum == 0) {
+					cell.innerHTML = 'S';
+					cell.className = 'w-16 h-16 text-center text-blue-500';
+					continue;
+				} else if (rowNum == rows - 1 && colNum == cols - 1) {
+					cell.innerHTML = 'F';
+					cell.className = 'w-16 h-16 text-center text-blue-500';
+					continue;
 				} else {
-					output += `<td class="w-3 h-5 text-center p-1">${maze[i][j]}</td>`;
+					cell.innerHTML = maze[rowNum][colNum];
+					cell.className = 'w-16 h-16 text-center';
+					cell.addEventListener('click', () => {
+						// Toggles location between wall and free space
+						if (maze[rowNum][colNum] === '#') {
+							maze[rowNum][colNum] = '.';
+							cell.innerHTML = '.';
+						} else {
+							maze[rowNum][colNum] = '#';
+							cell.innerHTML = '#';
+						}
+					});
 				}
 			}
-			output += '</tr>';
 		}
-		output += '</table>';
-		displayOutput(output);
 	}
 
-	function displayOutput(output: string) {
-		mazeOutput.innerHTML = output;
-		console.log(output);
+	function clear(): void {
+		maze = Array.from({ length: rows }, () => Array.from({ length: cols }, () => '.'));
+		printMaze();
 	}
+
+	function clearPath(): void {
+		for (let r = 0; r < rows; r++) {
+			for (let c = 0; c < cols; c++) {
+				if (maze[r][c] === '*') {
+					maze[r][c] = '.';
+				}
+			}
+		}
+		printMaze(); // Redraw the maze to reflect the cleared path
+	}
+
+	function changeDimension(): void {
+		rows = Number(rowInputEl.value);
+		cols = Number(colInputEl.value);
+		maze = Array.from({ length: rows }, () => Array.from({ length: cols }, () => '.'));
+		printMaze();
+	}
+
+	function changeAlgorithm(event: Event): void {
+		algorithm = (event.target as HTMLSelectElement).value;
+	}
+
+	onMount(() => {
+		printMaze();
+	});
 </script>
 
 <div class="mx-[10vw] flex w-[80vw] flex-col pt-10">
 	<h1 class="text-center text-3xl">Maze Solver</h1>
 
 	<div class="mt-4">
-		<div class="flex flex-row">
-			<div>
-				<h2 class="text-lg">Instructions</h2>
-				<p>
-					In order to use the maze solver, you can either upload a text file or design it in the
-					text box below.
-				</p>
-				<p>
-					The valid characters are '.', '#', 'S', and 'F'. '#' is used to represent a wall, '.'
-					represents an open space, and 'S' and 'F' represent the start and finish locations
-					respectively.
-				</p>
-				<p>When submitting a maze, make sure to remove any empty lines.</p>
-			</div>
-			<div class="ml-auto mr-0 text-lg">
-				<h2>Example Maze</h2>
-				<table>
-					<tr class="text-sm"
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">.</td><td
-							class="h-5 w-5 p-1 text-center">.</td
-						><td class="h-5 w-5 p-1 text-center">#</td><td class="h-5 w-5 p-1 text-center">#</td><td
-							class="h-5 w-5 p-1 text-center">#</td
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">#</td><td
-							class="h-5 w-5 p-1 text-center">.</td
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">.</td
-						></tr
-					><tr class="text-sm"
-						><td class="h-5 w-5 p-1 text-center">#</td><td class="h-5 w-5 p-1 text-center">#</td><td
-							class="h-5 w-5 p-1 text-center">.</td
-						><td class="h-5 w-5 p-1 text-center">#</td><td class="h-5 w-5 p-1 text-center">.</td><td
-							class="h-5 w-5 p-1 text-center">.</td
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">#</td><td
-							class="h-5 w-5 p-1 text-center">#</td
-						><td class="h-5 w-5 p-1 text-center">#</td><td class="h-5 w-5 p-1 text-center">#</td
-						></tr
-					><tr class="text-sm"
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">.</td><td
-							class="h-5 w-5 p-1 text-center">.</td
-						><td class="h-5 w-5 p-1 text-center">#</td><td class="h-5 w-5 p-1 text-center">.</td><td
-							class="h-5 w-5 p-1 text-center">#</td
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">#</td><td
-							class="h-5 w-5 p-1 text-center">.</td
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">.</td
-						></tr
-					><tr class="text-sm"
-						><td class="h-5 w-5 p-1 text-center">#</td><td class="h-5 w-5 p-1 text-center">.</td><td
-							class="h-5 w-5 p-1 text-center">#</td
-						><td class="h-5 w-5 p-1 text-center">#</td><td class="h-5 w-5 p-1 text-center">#</td><td
-							class="h-5 w-5 p-1 text-center">#</td
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">#</td><td
-							class="h-5 w-5 p-1 text-center">#</td
-						><td class="h-5 w-5 p-1 text-center">#</td><td class="h-5 w-5 p-1 text-center">#</td
-						></tr
-					><tr class="text-sm"
-						><td class="h-5 w-5 p-1 text-center">#</td><td class="h-5 w-5 p-1 text-center">F</td><td
-							class="h-5 w-5 p-1 text-center">.</td
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">#</td><td
-							class="h-5 w-5 p-1 text-center">.</td
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">#</td><td
-							class="h-5 w-5 p-1 text-center">.</td
-						><td class="h-5 w-5 p-1 text-center">#</td><td class="h-5 w-5 p-1 text-center">#</td
-						></tr
-					><tr class="text-sm"
-						><td class="h-5 w-5 p-1 text-center">#</td><td class="h-5 w-5 p-1 text-center">#</td><td
-							class="h-5 w-5 p-1 text-center">#</td
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">#</td><td
-							class="h-5 w-5 p-1 text-center">.</td
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">.</td><td
-							class="h-5 w-5 p-1 text-center">.</td
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">S</td
-						></tr
-					><tr class="text-sm"
-						><td class="h-5 w-5 p-1 text-center">#</td><td class="h-5 w-5 p-1 text-center">.</td><td
-							class="h-5 w-5 p-1 text-center">#</td
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">#</td><td
-							class="h-5 w-5 p-1 text-center">#</td
-						><td class="h-5 w-5 p-1 text-center">#</td><td class="h-5 w-5 p-1 text-center">#</td><td
-							class="h-5 w-5 p-1 text-center">.</td
-						><td class="h-5 w-5 p-1 text-center">#</td><td class="h-5 w-5 p-1 text-center">#</td
-						></tr
-					><tr class="text-sm"
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">.</td><td
-							class="h-5 w-5 p-1 text-center">.</td
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">#</td><td
-							class="h-5 w-5 p-1 text-center">.</td
-						><td class="h-5 w-5 p-1 text-center">#</td><td class="h-5 w-5 p-1 text-center">.</td><td
-							class="h-5 w-5 p-1 text-center">.</td
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">#</td
-						></tr
-					><tr class="text-sm"
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">#</td><td
-							class="h-5 w-5 p-1 text-center">#</td
-						><td class="h-5 w-5 p-1 text-center">#</td><td class="h-5 w-5 p-1 text-center">#</td><td
-							class="h-5 w-5 p-1 text-center">.</td
-						><td class="h-5 w-5 p-1 text-center">#</td><td class="h-5 w-5 p-1 text-center">.</td><td
-							class="h-5 w-5 p-1 text-center">#</td
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">#</td
-						></tr
-					><tr class="text-sm"
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">.</td><td
-							class="h-5 w-5 p-1 text-center">.</td
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">.</td><td
-							class="h-5 w-5 p-1 text-center">.</td
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">.</td><td
-							class="h-5 w-5 p-1 text-center">#</td
-						><td class="h-5 w-5 p-1 text-center">.</td><td class="h-5 w-5 p-1 text-center">.</td
-						></tr
-					>
-				</table>
-			</div>
-		</div>
+		<h2 class="text-blue text-lg">Instructions</h2>
+		<p>To use the maze solver, start designing a maze in the grid below.</p>
+		<p>
+			'#' is used to represent a wall, '.' represents an open space, and 'S' and 'F' represent the
+			start and finish locations respectively.
+		</p>
+		<p>After solving the maze, the valid path will be displayed using '*' characters.</p>
 	</div>
 
-	<div class="mt-10 flex flex-row">
-		<!-- Option to upload text file -->
-		<form>
-			<label for="file-input">Upload Maze File:<br /></label>
-			<input
-				type="file"
-				id="file-input"
-				accept=".txt"
-				on:change={uploadMazeFile}
-				bind:this={fileInput}
-			/>
-		</form>
+	<div class="flex flex-col items-center justify-center align-middle">
+		<table id="board" class="table w-fit text-3xl" />
 
-		<!-- Option to create own maze in website -->
-		<form id="maze-form" on:submit|preventDefault={processMazeInput} class="ml-auto mr-0">
-			<textarea
-				bind:this={mazeInput}
-				rows="10"
-				cols="20"
-				class="textarea-bordered textarea text-xs"
-			>...###.#....
-##.#...####.
-...#.#.#....
-#.####.####.
-#F..#..#.##.
-###.#....#S.
-#.#.####.##.
-....#.#...#.
-.####.#.#.#.
-........#...</textarea>
-			<br />
-			<button type="submit" class="btn">Solve Maze</button>
-		</form>
-	</div>
-
-	<div class="card mb-12 bg-base-100 shadow-xl">
-		<div class="card-body">
-			<h2 class="card-title">Output:</h2>
-			<div class="mx-auto">
-				<div id="maze-output" bind:this={mazeOutput} />
+		<div class="form-control mt-5 w-fit">
+			<label class="input-group">
+				<span>Algorithm:</span>
+				<select bind:value={algorithm} class="input-bordered input">
+					<option value="BFS">BFS</option>
+					<option value="DFS">DFS</option>
+				</select>
+				<span>Rows:</span>
+				<input
+					bind:this={rowInputEl}
+					type="number"
+					min="3"
+					max="20"
+					value="5"
+					class="input-bordered input"
+				/>
+				<span>Columns:</span>
+				<input
+					bind:this={colInputEl}
+					type="number"
+					min="3"
+					max="20"
+					value="5"
+					class="input-bordered input"
+				/>
+				<button on:click={changeDimension} class="btn-square btn w-fit px-3">Generate Board</button>
+			</label>
+			<div class="flex w-full flex-row justify-center space-x-[8%]">
+				<button on:click={solve} class="btn mt-5 w-5/12">Solve</button>
+				<button on:click={clear} class="btn mt-5 w-5/12">Clear</button>
 			</div>
 		</div>
 	</div>
